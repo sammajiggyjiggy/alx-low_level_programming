b1c3af6bc747a4c297c6f8d17014b060f44c9666
sammajiggyjiggy <samailajibrin.sj@gmail.com>
2022-09-09 00:23:01 +0100

    My first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Return: 0
+ * main - Prints "Programming is like building a multilingual puzzle.
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle.);
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
