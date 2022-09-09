4d78e16c5b3f63dfb9a30dc2b98d4c5e4c1d22b9
sammajiggyjiggy <samailajibrin.sj@gmail.com>
2022-09-09 01:12:52 +0100

    My first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19".
  * 
  * Return: Always 1.
  */
