7b355964d4c56f5c29dc4a67551d1a27b42d8250
sammajiggyjiggy <samailajibrin.sj@gmail.com>
2022-09-19 22:52:26 +0100

    My first commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,9 +12,9 @@ void puts_half(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 		i++
-	for (i /= 2; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
+			for (i /= 2; str[i] != '\0'; i++)
+			{
+				_putchar(str[i]);
+			}
 	_putchar('\n');
 }
