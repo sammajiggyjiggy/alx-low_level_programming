dcb453907464fca1385cd4d10eb26548b1727418
sammajiggyjiggy <samailajibrin.sj@gmail.com>
2022-09-27 21:24:58 +0100

    My first commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _memset - fills memory with a constant byte
- * @s: pointer block of memory to fill
- * @b: value to set
- * @n: bytes of the memory
- * Return: dest
+ *_memset - fills memory with a constant byte
+ *@s: pointer block of memory to fill
+ *@b: value to set
+ *@n: bytes of the memory
+ *Return: dest
  */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -12,7 +12,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	/*Declaring FOR*/
+	/*Delacring FOR*/
 	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b; /*add 1 position s*/
