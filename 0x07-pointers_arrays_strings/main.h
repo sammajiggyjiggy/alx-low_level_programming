c9f75b3fc259738a39bec797a541928e1b79f333
sammajiggyjiggy <samailajibrin.sj@gmail.com>
2022-09-27 21:54:14 +0100

    My first commit

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * void prototypes()
+ * int prototypes()
+ */
+
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -7,4 +15,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[a]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-int _putchar(char c);
+int _putchar(char);
