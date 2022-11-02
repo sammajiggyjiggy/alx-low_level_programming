9ac7399e578c31ad2d3b1fb829e92bacac73b3f5
sammajiggyjiggy <samailajibrin.sj@gmail.com>
2022-11-02 16:58:18 +0100

    My first commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,24 +11,24 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-int i;
+	int i;
 
-if (*needle == 0)
-	return (haystack);
+	if (*needle == 0)
+		return (haystack);
 
-while (*haystack)
-{
-	i = 0;
-
-	if (haystack[i] == needle[i])
+	while (*haystack)
 	{
-		do {
-			if (needle[i + 1] == '\0')
-				return (haystack);
-			i++;
-		} while (haystack[i] == needle[i]);
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
+		}
+		haystack++;
 	}
-	haystack++;
-}
-return ('\0);
+	return ('\0');
 }
